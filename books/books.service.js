e2262e200bc3a5d9e15c2ddbdd9178d8a2b46b19
divyamdinker007 <71220418+divyamdinker007@users.noreply.github.com>
2023-04-21 13:39:49 +0530

    second

--- books/books.service.js
+++ books/books.service.js
@@ -1,5 +1,15 @@
-module.exports.create = function(payload){}
+const Book = require("./Book")
+
+module.exports.create = function(payload){
+    
+}
 module.exports.update = function(id, payload){}
-module.exports.delete = function(id){}
-module.exports.list = function(){}
-module.exports.get = function(id){}
+module.exports.delete = async function(id){
+    await Book.findByIdAndDelete({_id:id})
+}
+module.exports.list = async function(){
+   return await Book.find()
+}
+module.exports.get = async function(id){
+    return await Book.finfById({_id:id})
+}
